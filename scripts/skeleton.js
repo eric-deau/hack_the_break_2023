76714aae5c6aa443a6d1dac3dd30499b665e360d
ALEX DESCHENES <adeschenes@my.bcit.ca>
2023-03-11 10:01:04 -0800

    add skeleton.js

---
+++ scripts/skeleton.js
@@ -0,0 +1,8 @@
+function loadSkeleton() {
+    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#mapPlaceholder').load('./text/map.html'));
+    console.log($('#footerBeforeLoginPlaceholder').load('./text/footer_before_login.html'));
+    console.log($('#footerAfterLoginPlaceholder').load('./text/footer_after_login.html'));
+    console.log($('#reportPlaceholder').load('./text/submission.html'));
+}
+loadSkeleton();
