da5a221a49614f3b0123baea948370e6ac94e532
ERIC DEAU <edeau@my.bcit.ca>
2023-03-11 11:39:04 -0800

    Add functional top nav and updated template.html

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,8 +1,4 @@
 function loadSkeleton() {
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#mapPlaceholder').load('./text/map.html'));
-    console.log($('#footerBeforeLoginPlaceholder').load('./text/footer_before_login.html'));
-    console.log($('#footerAfterLoginPlaceholder').load('./text/footer_after_login.html'));
-    console.log($('#reportPlaceholder').load('./text/submission.html'));
+    console.log($('#navbarPlaceholder').load('./constants/navbar.html'));
 }
 loadSkeleton();
