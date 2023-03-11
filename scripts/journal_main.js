125d4fe9c7f8eda7068fa828c2d244ed8b295e3e
NATHAN YAU <nshyau@gmail.com>
2023-03-11 12:24:54 -0800

    Add journal display

--- scripts/journal_main.js
+++ scripts/journal_main.js
@@ -13,14 +13,32 @@ function displayJournalDynamically(collection) {
                 let newcard = cardTemplate.content.cloneNode(true);
                 var d = new Date(timestamp.seconds * 1000)
                 var postID = doc.id
+                switch (tag) {
+                    case "Happy":
+                        var emoji = "😀"
+                        break;
+                    case "Sad":
+                        var emoji = "😢"
+                        break;
+                    case "Angry":
+                        var emoji = "😡"
+                        break;
+                    case "Fear":
+                        var emoji = "😨"
+                        break;
+                    case "Disgust":
+                        var emoji = "🤢"
+                        break;
+                }
                 // //update title and text and image
                 newcard.querySelector('#journal-content').innerHTML = content;
                 newcard.querySelector('#journal-picture').src = picture;
                 // newcard.querySelector('.postcontent').innerHTML = postcontent;
-                newcard.querySelector('#journal-timestamp').innerHTML = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " +
+                newcard.querySelector('#journal-timestamp').innerHTML = emoji + " " + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " +
                     d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
                 newcard.querySelector('#journal-tag').innerHTML = tag;
                 newcard.querySelector('#journal-tag').classList.add("tag-"+tag);
+                newcard.querySelector('#journalCard').classList.add("journal-"+tag+"-mood");
                 newcard.querySelector('#journal-tip').classList.add("tag-" + tag);
                 document.getElementById("previous-journal-go-here").appendChild(newcard);
                 console.log("HI")
