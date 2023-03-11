dda9d648f9827bf11b2a9e5fc1a0c0a0aed3c567
ERIC DEAU <edeau@my.bcit.ca>
2023-03-11 14:06:21 -0800

    Add login functionality to login.html

---
+++ scripts/authentication.js
@@ -0,0 +1,39 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            var user = authResult.user;
+            if (authResult.additionalUserInfo.isNewUser) {
+                db.collection("users").doc(user.uid).set({
+                    name: user.displayName,
+                    email: user.email,
+                }).then(function () {
+                    console.log("New user added to firestore");
+                    window.location.assign("main.html");
+                }).catch(function (error) {
+                    console.log("Error adding new user: " + error);
+                });
+            } else {
+                return true;
+            }
+            return false;
+        },
+
+        uiShown: function () {
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+
+    signInFlow: 'popup',
+    signInSuccessUrl: './main.html',
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+
+    tosUrl: '<your-tos-url>',
+
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
